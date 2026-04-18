5a94ad2ab7f2b42a44f108abc8ccdc885f3dbbf6
zernonia <zernonia@gmail.com>
2026-04-18 17:28:23 +0800

    chore: adjust tabslist variants

--- apps/v4/styles/reka-luma/ui/tabs/TabsList.vue
+++ apps/v4/styles/reka-luma/ui/tabs/TabsList.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import type { TabsListProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
+import type { TabsListVariants } from '.'
 import { reactiveOmit } from '@vueuse/core'
 import { TabsList } from 'reka-ui'
 import { cn } from '@/lib/utils'
+import { tabsListVariants } from '.'
 
 const props = withDefaults(defineProps<TabsListProps & {
   class?: HTMLAttributes['class']
-  variant?: 'default' | 'line'
+  variant?: TabsListVariants['variant']
 }>(), {
   variant: 'default',
 })
@@ -20,12 +22,7 @@ const delegatedProps = reactiveOmit(props, 'class', 'variant')
     data-slot="tabs-list"
     :data-variant="variant"
     v-bind="delegatedProps"
-    :class="cn(
-      'rounded-full p-1 group-data-horizontal/tabs:h-9 group-data-vertical/tabs:rounded-2xl data-[variant=line]:rounded-none group/tabs-list inline-flex w-fit items-center justify-center text-muted-foreground group-data-vertical/tabs:h-fit group-data-vertical/tabs:flex-col',
-      variant === 'default' && 'bg-muted',
-      variant === 'line' && 'gap-1 bg-transparent',
-      props.class,
-    )"
+    :class="cn(tabsListVariants({ variant }), props.class)"
   >
     <slot />
   </TabsList>
