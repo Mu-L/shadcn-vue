8005b7d8783cda97e6e2a0898174c82ce1f45f86
zhengpeng <40790268+linzhe141@users.noreply.github.com>
2026-05-22 20:45:50 +0800

    fix(chart-legend): avoid undefined type by using value (#1749)
    * fix(chart-legend): avoid undefined type by using value from Object.entries
    
    * chore: update

--- apps/v4/registry/new-york-v4/ui/chart/ChartLegendContent.vue
+++ apps/v4/registry/new-york-v4/ui/chart/ChartLegendContent.vue
@@ -19,7 +19,7 @@ const { id, config } = useChart()
 const payload = computed(() => Object.entries(config.value).map(([key, value]) => {
   return {
     key: props.nameKey || key,
-    itemConfig: config.value[key],
+    itemConfig: value,
   }
 }))
 
@@ -45,7 +45,7 @@ onMounted(() => {
         '[&>svg]:text-muted-foreground flex items-center gap-1.5 [&>svg]:h-3 [&>svg]:w-3',
       )"
     >
-      <component :is="itemConfig?.icon" v-if="itemConfig?.icon" />
+      <component :is="itemConfig.icon" v-if="itemConfig.icon" />
       <div
         v-else
         class="h-2 w-2 shrink-0 rounded-[2px]"
@@ -54,7 +54,7 @@ onMounted(() => {
         }"
       />
 
-      {{ itemConfig?.label }}
+      {{ itemConfig.label }}
     </div>
   </div>
 </template>
